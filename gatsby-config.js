f989e926406f6b65652d19335632eef8df1ca785
Arun Sondhi <aksondhi@gmail.com>
2022-06-02 11:13:41 -0400

    update og thumbnail data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Arun Sondhi',
     description:
-      'Arun Sondhi is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Arun Sondhi is a Senior Software Engineer who loves researching and learning, 3D computer vision, leveraging GPU acceleration, and automation at scale.',
     siteUrl: 'https://arunsondhi.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@aksondev',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
